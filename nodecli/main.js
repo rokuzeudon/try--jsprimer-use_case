6fb72363faa72df9ed4c8436171b75e774130faf
rokuzeudon <g-awaya@6q.design>
2019-11-16 22:48:54 +0900

    Node.jsでCLIアプリケーション: コマンドライン引数を処理する
    https://jsprimer.net/use-case/nodecli/argument-parse/
    - process.argv配列にnodeコマンドのコマンドライン引数が格納されていることを確認した
    - npmを使ってパッケージをインストールする方法を理解した
    - require関数を使ってパッケージのモジュールを読み込めることを確認した
    - commanderを使ってコマンドライン引数をパースできることを確認した
    - コマンドライン引数で渡されたファイルパスを取得してコンソールに出力できた

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+// commanderモジュールをprogramとしてインポートする
+const program = require("commander");
+// コマンドライン引数をcommanderでパースする
+program.parse(process.argv);
+
+// ファイルパスをprogram.args配列から取り出す
+const filePath = program.args[0];
+console.log(filePath);
