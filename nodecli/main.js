50688858e468eb27184bcbd3b057d7de6d46cec9
rokuzeudon <g-awaya@6q.design>
2019-11-16 22:52:30 +0900

    Node.jsでCLIアプリケーション: ファイルを読み込む
    https://jsprimer.net/use-case/nodecli/read-file/
    - fsモジュールのreadFile関数を使ってファイルを読み込んだ
    - UTF-8形式のファイルの中身をコンソールに出力した
    - readFile関数の呼び出しにエラーハンドリング処理を記述した

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,8 +1,22 @@
 // commanderモジュールをprogramとしてインポートする
 const program = require("commander");
-// コマンドライン引数をcommanderでパースする
+
+// fsモジュールをfsオブジェクトとしてインポートする
+const fs = require("fs");
+
+// コマンドライン引数からファイルパスを取得する
 program.parse(process.argv);
 
 // ファイルパスをprogram.args配列から取り出す
 const filePath = program.args[0];
-console.log(filePath);
+
+// ファイルを非同期で読み込む
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+  if (err) {
+    console.error(err.message);
+    // 終了ステータス 1（一般的なエラー）としてプロセスを終了する
+    process.exit(1);
+    return;
+  }
+  console.log(file);
+});
