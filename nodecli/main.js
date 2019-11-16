0ad2bfb96f5129e03e9e7bc7838778dc9e1643e6
rokuzeudon <g-awaya@6q.design>
2019-11-16 22:57:52 +0900

    Node.jsでCLIアプリケーション: MarkdownをHTMLに変換する
    https://jsprimer.net/use-case/nodecli/md-to-html/
    - markedパッケージを使ってMarkdown文字列をHTML文字列に変換した
    - コマンドライン引数でmarkedの変換オプションを設定した
    - デフォルトオプションを定義し、コマンドライン引数で上書きできるようにした

--- nodecli/main.js
+++ nodecli/main.js
@@ -4,11 +4,23 @@ const program = require("commander");
 // fsモジュールをfsオブジェクトとしてインポートする
 const fs = require("fs");
 
+// markedモジュールをmarkedオブジェクトとしてインポートする
+const marked = require("marked");
+
+// gfmオプションを定義する
+program.option("--gfm", "GFMを有効にする");
+
 // コマンドライン引数からファイルパスを取得する
 program.parse(process.argv);
 
 // ファイルパスをprogram.args配列から取り出す
 const filePath = program.args[0];
+
+// コマンドライン引数のオプションを取得し、デフォルトのオプションを上書きする
+const cliOptions = {
+  gfm: false,
+  ...program.opts(),
+};
 
 // ファイルを非同期で読み込む
 fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
@@ -18,5 +30,10 @@ fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
     process.exit(1);
     return;
   }
-  console.log(file);
+  // MarkdownファイルをHTML文字列に変換する
+  const html = marked(file, {
+    // オプションの値を使用する
+    gfm: cliOptions.gfm,
+  });
+  console.log(html);
 });
